883141239b13344e740ffacaffa4589e9c9d47e5
belle <belle.baxley@gmail.com>
2023-02-02 16:05:43 -0500

    first commit

---
+++ Lab12 Data Structures/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Write("Please enter a word: ");
+string userWord = Console.ReadLine();
+
+
+static string ReverseString(string input)
+{
+   
+    char[] letters = input.ToCharArray();
+
+    Stack<char> lettersStack = new Stack<char>();
+
+    foreach (char c in letters)
+    {
+        lettersStack.Push(c);
+    }
+
+    foreach (char c in lettersStack)
+    {
+        Console.Write(c);
+    }
+
+    string output = " ";
+    return output;
+
+}
+
+ReverseString(userWord);
